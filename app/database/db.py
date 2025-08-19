4962ccefec2cde83bff499219c5a17736cc17102
bhavindev <bhavinsen1992@gmail.com>
2025-08-19 12:59:32 +0530

    database connection

---
+++ app/database/db.py
@@ -0,0 +1,18 @@
+from sqlmodel import create_engine, Session, SQLModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(bind=engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
